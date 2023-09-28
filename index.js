e35eaf282a7d828b1274a3d3725b5928bd540b64
Dmytro Haidai <dimasow12@gmail.com>
2023-09-28 11:48:45 +0300

    cmt1

---
+++ index.js
@@ -0,0 +1,29 @@
+import contactsService from "./contacts.js";
+import yargs from "yargs";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      return console.log(oneContact);
+    case "add":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(newContact);
+    case "remove":
+      const removeContact = await contactsService.removeContact(id);
+      return console.log(removeContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const { argv } = yargs(process.argv.slice(2));
+invokeAction(argv);
